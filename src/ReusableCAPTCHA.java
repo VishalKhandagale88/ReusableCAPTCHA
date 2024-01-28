af0cf3e1e5a3c1196202f52555f2a0f6a5a9f885
vishal <vishal88kh@gmail.com>
2024-01-28 16:24:25 +0530

    Initial commit

---
+++ src/ReusableCAPTCHA.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class ReusableCAPTCHA extends JFrame {
+    private JLabel captchaLabel;
+    private JTextField captchaTextField;
+    private JButton generateButton;
+    private String generatedCaptcha;
+
+  
+}
