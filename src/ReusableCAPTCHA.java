abdeceb78106a95033cec1cf548e431c028704f3
vishal <vishal88kh@gmail.com>
2024-01-28 16:27:58 +0530

    added title, size and default frame

--- src/ReusableCAPTCHA.java
+++ src/ReusableCAPTCHA.java
@@ -5,5 +5,10 @@ public class ReusableCAPTCHA extends JFrame {
     private JButton generateButton;
     private String generatedCaptcha;
 
-  
+    public ReusableCAPTCHA(){
+        setTitle("Captcha Generator");
+        setSize(500,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
